d3cac764789fa51d254b45936fd088caf99efe54
Muhammad Kaleem <52773343+mkaleempk@users.noreply.github.com>
2025-08-12 08:28:49 +0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,20 +1,20 @@
-# app/app.py
-
 from flask import Flask
 import psycopg2
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def students():
     conn = psycopg2.connect(
-        host = 'db', dbname='students', user='admin', password='admin123'
+        host='db', dbname='students', user='admin', password='admin123'
     )
     cur = conn.cursor()
     cur.execute("SELECT name FROM student")
     rows = cur.fetchall()
+    cur.close()
+    conn.close()
     return '<br>'.join(name for (name,) in rows)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
