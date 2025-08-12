5b95ca65855a65a6acea16849bbabb99c0edd710
Muhammad Kaleem <52773343+mkaleempk@users.noreply.github.com>
2025-08-12 07:40:41 +0500

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,8 +1,11 @@
 -- db/init.sql
-
 CREATE TABLE if not EXISTS student (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(100)
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100)
 );
 
-INSERT INTO student (name) VALUES ('Ali'), ('Sara'), ('Ahmed');
+INSERT INTO student (name) VALUES ('Ali'), ('Sara'), ('Ahmed')
+ON CONFLICT DO NOTHING;
+
+-- Show the table contents
+SELECT * FROM student;
